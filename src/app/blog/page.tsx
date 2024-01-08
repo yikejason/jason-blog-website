1fbbc3825b9ffc84bd2a0cb6f1d57c662842d015
yikejason <18215513386@163.com>
2024-01-08 15:58:11 +0800

    added blog page layout

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const page = () => {
-  return <div>22</div>;
+  return (
+    <main className="flex min-h-screen flex-col">
+      <div className="container mx-auto px-12 py-4 mt-24">111</div>
+    </main>
+  );
 };
 
 export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-white">
+    <main className="flex min-h-screen flex-col">
       <div className="container mx-auto px-12 py-4 mt-24"></div>
     </main>
   );
