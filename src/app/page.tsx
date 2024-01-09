f264a96bb22af0301d447efd09587db8862dffcf
yikejason <18215513386@163.com>
2024-01-09 14:34:34 +0800

    added basicLayout page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import BasicLayout from '@/src/components/Layout';
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col">
-      <div className="container mx-auto px-12 py-4 mt-24"></div>
-    </main>
+    <BasicLayout>
+      <h2 className="text-3xl md:text-4xl text-black">关于</h2>
+      <p className="text-gray-600 max-w-5xl">
+        专注于前端的全栈工程师，参与工作距今已经有十多年，有着丰富的2B系统开发及架构经验。早年从事UI设计工作，因此对用户体验及交互上具有一定的把控。目前主要以前端开发为主，对服务端技术如：nodejs、next、nest也具备一定的掌握。擅长以React为核心的各类技术栈进行软件开发，喜欢工程化方面领域，喜欢交互设计。
+      </p>
+    </BasicLayout>
   );
 }
 
---
+++ src/components/Layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <div className="container mx-auto px-4 py-4 mt-24">
+        <section className="max-w-prose grid gap-9 m-auto text-gray-600 leading-relaxed">
+          {children}
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
+
